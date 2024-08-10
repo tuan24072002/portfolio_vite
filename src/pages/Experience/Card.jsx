3cb39281c6b9362c34620feefd1672898c1d963b
tuan24072002 <44106906+tuan24072002@users.noreply.github.com>
2024-08-10 22:55:54 +0700

    new commit

--- src/pages/Experience/Card.jsx
+++ src/pages/Experience/Card.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-const Card = ({ year, title, desc, desc2, icon }) => {
+const Card = (props) => {
+    const { year, title, desc, desc2, icon } = props
     return (
         <div className="relative pl-12 pb-12 last:pb-0 before:content-[''] before:w-[1px] before:h-full before:bg-[hsl(353,100%,65%)] before:absolute before:left-1">
             <i className="absolute -left-[0.4375rem] top-0 text-3xl text-[hsl(353,100%,65%)] bg-[var(--container-color)] py-[0.4375rem]">{icon}</i>
